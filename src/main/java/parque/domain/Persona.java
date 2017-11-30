bf1c05768fd450ffc2c27486439bdede9820814e
Maria Alejandra Rendon Baena - Ceiba Software <maria.rendon@ceiba.com.co>
2017-11-30 15:43:34 -0500

    Se agrega la clase persona al src/main/java

---
+++ src/main/java/parque/domain/Persona.java
@@ -0,0 +1,23 @@
+package parque.domain;
+
+public class Persona {
+	
+	private String nombre;
+	private String identificación;
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public String getIdentificación() {
+		return identificación;
+	}
+	public void setIdentificación(String identificación) {
+		this.identificación = identificación;
+	}
+	
+	
+	
+
+}

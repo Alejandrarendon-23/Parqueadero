22eb63a8b118582cd71fa12828a773094e3af884
Maria Alejandra Rendon Baena - Ceiba Software <maria.rendon@ceiba.com.co>
2017-11-30 11:38:56 -0500

    Se agrega la clase vehiculo dentro del src/main

---
+++ src/main/java/parque/domain/Vehiculo.java
@@ -0,0 +1,48 @@
+package parque.domain;
+
+public class Vehiculo {
+	
+	private String tipo;
+	private String modelo;
+	private String marca;
+	private String placa;
+	private Boolean estado;
+	public String getTipo() {
+		return tipo;
+	}
+	public void setTipo(String tipo) {
+		this.tipo = tipo;
+	}
+	public String getModelo() {
+		return modelo;
+	}
+	public void setModelo(String modelo) {
+		this.modelo = modelo;
+	}
+	public String getMarca() {
+		return marca;
+	}
+	public void setMarca(String marca) {
+		this.marca = marca;
+	}
+	public String getPlaca() {
+		return placa;
+	}
+	public void setPlaca(String placa) {
+		this.placa = placa;
+	}
+	public Boolean getEstado() {
+		return estado;
+	}
+	public void setEstado(Boolean estado) {
+		this.estado = estado;
+	}
+	
+	
+	
+
+	
+
+	
+
+}

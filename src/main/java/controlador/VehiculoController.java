1424820089eccfacdc1ed0365ee14d5717061970
Maria Alejandra Rendon Baena - Ceiba Software <maria.rendon@ceiba.com.co>
2017-12-18 16:13:46 -0500

    Se solucionan issues y se optimizan metodos

--- src/main/java/controlador/VehiculoController.java
+++ src/main/java/controlador/VehiculoController.java
@@ -11,10 +11,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import dominio.Carro;
 import dominio.CeldaParqueo;
-import dominio.Moto;
-import dominio.Vehiculo;
 import dominio.repositorio.RepositorioCelda;
 import persistencia.sistema.SistemaPersistencia;
 import servicio.AdminParqueaderoServicio;
